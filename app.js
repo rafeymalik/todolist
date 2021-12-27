95dabd171a83491c64fea28a882f601e0c142f92
hammadpathan <96709353+hammadpathan@users.noreply.github.com>
2021-12-26 22:05:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+const adder = document.querySelector('.submitbutton');
+
+switcher.addEventListener('click',function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+
+});
+
+adder.addEventListener('click',function() {
+    document.getElementById("firstitem").textContent = document.getElementById("thingy").value;
+})
+
